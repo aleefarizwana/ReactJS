03f56dcf083b9939f9f86e3ab02b896c215b0f81
aleefarizwana <126440963+aleefarizwana@users.noreply.github.com>
2023-08-30 22:01:34 +0530

    Section2.js
    code to display section2

---
+++ my-app/src/components/about/Section2.js
@@ -0,0 +1,23 @@
+// import React from 'react'
+// import "../../styles/section1.css"
+// const Section2 =()=>{
+//     return(
+//         <div className="section">
+//             <div className="text">
+//                 Hello1
+//             </div>
+//             <div className="text">
+//                 Hiiiii
+//             </div>
+//         </div>
+//     )
+// }
+// export default Section2
+import React from 'react'
+
+const Section2 = () => {
+    return(
+        <div>section2</div>
+    );
+}
+export default Section2
