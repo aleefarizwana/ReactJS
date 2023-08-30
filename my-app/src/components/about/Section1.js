7dcbdff78f35ff0db0e50f2d516ad21d47256d57
aleefarizwana <126440963+aleefarizwana@users.noreply.github.com>
2023-08-30 21:59:59 +0530

    Section1.js
    code to display as section1

---
+++ my-app/src/components/about/Section1.js
@@ -0,0 +1,9 @@
+//code display section1
+import React from 'react'
+import '../../styles/section1.css' 
+const Section1 = () => {
+    return(
+        <div>section1</div>
+    );
+}
+export default Section1
