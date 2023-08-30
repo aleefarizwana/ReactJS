cad52ebd9ffc33cf0b5f2c14592a0172a13decd8
aleefarizwana <126440963+aleefarizwana@users.noreply.github.com>
2023-08-30 22:09:25 +0530

    Pa3.jsx
    Another child having button to change the property place

---
+++ my-app/src/Pages/Pa3.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Pa3=({place3,onchangePro})=>{
+    return(
+        <div>
+            <h2>pa3 child Component</h2>
+            <p>{place3}</p>
+            <button onClick={onchangePro}>place3</button>
+
+            
+        </div>
+    )
+}
+export default Pa3
