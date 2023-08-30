3eb415f994e8b9afd2a5f2b323ff8fe11b0131da
aleefarizwana <126440963+aleefarizwana@users.noreply.github.com>
2023-08-30 22:04:27 +0530

    About.jsx
    here we import the section1,2 code then return section1,2 from return statement.when we call About from App.js,we get output....

---
+++ my-app/src/Pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Section1 from '../components/about/Section1'
+import Section2 from '../components/about/Section2'
+
+
+const About=()=>{
+    return <>
+        <div>
+            <section1/>
+            <section2/>
+         
+            </div>    
+         </>
+}
+export default About
